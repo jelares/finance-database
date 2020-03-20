1fd34615830b3352de5d8758777988aff584ce12
Enrique Avina <enriqueavina@Enriques-MacBook-Air.local>
2020-03-20 00:25:04 -0700

    .DS_Store banished!

--- src/databaseManager/Update.py
+++ src/databaseManager/Update.py
@@ -1,16 +1,37 @@
-import databaseManager.Utilities as util
+# import databaseManager.Utilities as util
+import Utilities as util
 
 '''
-
+Test File for Utility functions
 '''
 
 DATABASE_URL = "../../database/stocksInfo.db"
 
 table_name = "stocks"
-values = ["company", "date_stamp", "stock_price", "market_capitalization"]
-data_types = ["TEXT", "TEXT", "REAL", "REAL"]
+columns = {"company": "TEXT",
+           "date_stamp": "TEXT",
+           "stock_price": "REAL",
+           "market_capitalization": "REAL"}
 
-util.create_table(table_name, values, data_types, DATABASE_URL, True)
+util.create_table(table_name, list(columns.keys()), list(columns.values()), DATABASE_URL, True)
 
 curr_vals = ["Goldman", "3-19-2020", "50", "500"]
-util.data_entry(table_name, curr_vals, data_types, DATABASE_URL, True)
+util.data_entry(table_name, curr_vals, columns, DATABASE_URL, True)
+
+curr_vals = ["Goldman", "3-20-2020", "52", "510"]
+util.data_entry(table_name, curr_vals, columns, DATABASE_URL, True)
+
+curr_vals = ["Goldman", "3-21-2020", "51", "509"]
+util.data_entry(table_name, curr_vals, columns, DATABASE_URL, True)
+
+
+
+data = util.data_query(table_name, "Goldman", DATABASE_URL, [None, "3-21-2020"], True)
+# print(data)
+
+dates = [row[1] for row in data]
+stock_prices = [row[2] for row in data]
+
+print(dates)
+print(stock_prices)
+util.create_scatterplot("Goldman", dates, stock_prices)
--- src/databaseManager/Utilities.py
+++ src/databaseManager/Utilities.py
@@ -1,4 +1,5 @@
 import sqlite3
+import matplotlib.pyplot as plt
 
 '''
 Utilities file to interface with the database specified by its relative path
@@ -42,19 +43,25 @@ def create_table(table_name, values, data_types, database_path, debug=False):
     conn.close()
 
 
-def data_entry(table_name, values, data_types, database_path, debug=False):
+def data_entry(table_name, values, table_data, database_path, debug=False):
     """ Insert values into table_name in DB at database_path """
     conn = sqlite3.connect(database_path)
     c = conn.cursor()
 
-    entry_info = ("INSERT INTO " + table_name + " VALUES(")
+    entry_info = "INSERT INTO " + table_name + "("
+    for column in table_data:
+        entry_info += column + ", "
+
+    entry_info = entry_info[:-2] + ") VALUES("
+
+    data_types = list(table_data.values())
 
     for i in range(len(values)):
         val = values[i]
         val_type = data_types[i]
 
         if val_type == "TEXT":
-            entry_info += "'" + val + "', "
+            entry_info += "'" + val.lower() + "', "
         else:
             entry_info += val + ", "
 
@@ -68,3 +75,57 @@ def data_entry(table_name, values, data_types, database_path, debug=False):
     conn.commit()
     conn.close()
 
+
+def data_query(table_name, company, database_path, date_range=None, debug=False):
+    """
+    Query data from `company` from table `table_name` database at `database_path`
+
+    @param table_name string, the name of the table
+    @param company string, the name of the company to query
+    @param database_path string, the relative path of the database
+    @param date_range optional (start, end) a tuple of values to indicate date range for query
+           Either start of end can be left None, corresponding to dates less or greater than
+           a date, respectively. Dates must be entered in the format m-d-YYYY
+    @param debug boolean whether or not to print debug statements
+    @return An array of tuples representing query results
+    """
+
+    conn = sqlite3.connect(database_path)
+    c = conn.cursor()
+
+    entry_info = "SELECT * from " + table_name + " WHERE COMPANY = '" + company.lower() + "'"
+
+    if date_range is not None:
+        if date_range[0] is not None and date_range[1] is not None:
+            entry_info += " AND date_stamp BETWEEN '" + date_range[0] + "' and '" + date_range[1] + "'"
+        elif date_range[0] is not None:
+            entry_info += " AND date_stamp >= '" + date_range[0] + "'"
+        elif date_range[1] is not None:
+            entry_info += " AND date_stamp <= '" + date_range[1] + "'"
+
+    if debug:
+        print(entry_info)
+
+    data = c.execute(entry_info)
+    ret_data = [row for row in data]
+
+    conn.close()
+    return ret_data
+
+
+def create_scatterplot(company, dates, y_values):
+    """
+    Create a scatter plot plotting `dates` against `y_values`
+
+    @param company: The name of the company to plot
+    @param dates: An array of date strings in the format 'm-d-YYYY'
+            ex: ['3-20-2020', '3-21-2020']
+    @param y_values: An array of values to plot
+    """
+
+    # plot
+    plt.title(company)
+    plt.xlabel("Date")
+    plt.ylabel("Stock Price")
+    plt.scatter(dates, y_values)
+    plt.show()
