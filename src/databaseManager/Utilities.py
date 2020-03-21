2b0c97ae83a73556102b2a849fc4c009eb4fcbba
sidereal <jesus.elares@gmail.com>
2020-03-21 11:46:22 -0400

    updated scraper and changed table_data

--- src/databaseManager/Utilities.py
+++ src/databaseManager/Utilities.py
@@ -1,11 +1,12 @@
 import sqlite3
+import matplotlib.pyplot as plt
 
 '''
 Utilities file to interface with the database specified by its relative path
 '''
 
 
-def create_table(table_name, values, data_types, database_path, debug=False):
+def create_table(table_name, values, table_data, database_path, debug=False):
     """
     Method to create a database at database_path (if one does not already
     exist) and populate it with a table table_name with values values of type
@@ -13,7 +14,7 @@ def create_table(table_name, values, data_types, database_path, debug=False):
 
     @param table_name string, the name of the table
     @param values list of string column names in table
-    @param data_types list of equal size to values of column data type strings
+    @param table_data list of tuples (col_name, data_type) strings
     @param database_path string, the relative path of the database
     @param debug boolean whether or not to print debug statements
     """
@@ -25,7 +26,7 @@ def create_table(table_name, values, data_types, database_path, debug=False):
 
     for i in range(len(values)):
         val = values[i]
-        val_type = data_types[i]
+        val_type = table_data[i][1]
 
         create_info += val + " "
         create_info += val_type + ", "
@@ -42,20 +43,33 @@ def create_table(table_name, values, data_types, database_path, debug=False):
     conn.close()
 
 
-def data_entry(table_name, values, data_types, database_path, debug=False):
-    """ Insert values into table_name in DB at database_path """
+def data_entry(table_name, values, table_data, database_path, debug=False):
+    """
+    Insert a single row of values into table_name within DB at database_path
+
+    :param table_name: str name of table
+    :param values: list of string values for this row
+    :param table_data: list of tuples (col_name, data_type) strings
+    :param database_path: rel path of database
+    :param debug: flag whether or not to print debug data
+    """
     conn = sqlite3.connect(database_path)
     c = conn.cursor()
 
-    entry_info = ("INSERT INTO " + table_name + " VALUES(")
+    entry_info = "INSERT INTO " + table_name + "("
+    for column in table_data:
+        entry_info += column[0] + ", "
+
+    entry_info = entry_info[:-2] + ") VALUES("
 
     for i in range(len(values)):
         val = values[i]
-        val_type = data_types[i]
+        val_type = table_data[i][1]
 
         if val_type == "TEXT":
-            entry_info += "'" + val + "', "
+            entry_info += "'" + val.lower() + "', "
         else:
+            val = val.replace(",", "")
             entry_info += val + ", "
 
     entry_info = entry_info[0:-2] + ")"
@@ -68,3 +82,58 @@ def data_entry(table_name, values, data_types, database_path, debug=False):
     conn.commit()
     conn.close()
 
+
+# should probably include querying methods in the prediction module - Jesus
+def data_query(table_name, company, database_path, date_range=None, debug=False):
+    """
+    Query data from `company` from table `table_name` database at `database_path`
+
+    @param table_name string, the name of the table
+    @param company string, the name of the company to query
+    @param database_path string, the relative path of the database
+    @param date_range optional (start, end) a tuple of values to indicate date range for query
+           Either start of end can be left None, corresponding to dates less or greater than
+           a date, respectively. Dates must be entered in the format 'm-d-YYYY'
+    @param debug boolean whether or not to print debug statements
+    @return An array of tuples representing query results
+    """
+
+    conn = sqlite3.connect(database_path)
+    c = conn.cursor()
+
+    entry_info = "SELECT * from " + table_name + " WHERE COMPANY = '" + company.lower() + "'"
+
+    if date_range is not None:
+        if date_range[0] is not None and date_range[1] is not None:
+            entry_info += " AND date_stamp BETWEEN '" + date_range[0] + "' and '" + date_range[1] + "'"
+        elif date_range[0] is not None:
+            entry_info += " AND date_stamp >= '" + date_range[0] + "'"
+        elif date_range[1] is not None:
+            entry_info += " AND date_stamp <= '" + date_range[1] + "'"
+
+    if debug:
+        print(entry_info)
+
+    data = c.execute(entry_info)
+    ret_data = [row for row in data]
+
+    conn.close()
+    return ret_data
+
+
+def create_scatterplot(company, dates, y_values):
+    """
+    Create a scatter plot plotting `dates` against `y_values`
+
+    @param company: The name of the company to plot
+    @param dates: An array of date strings in the format 'm-d-YYYY'
+            ex: ['3-20-2020', '3-21-2020']
+    @param y_values: An array of values to plot
+    """
+
+    # plot
+    plt.title(company)
+    plt.xlabel("Date")
+    plt.ylabel("Stock Price")
+    plt.scatter(dates, y_values)
+    plt.show()
---
+++ src/scraping/scraper.py
@@ -0,0 +1,142 @@
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import databaseManager.Utilities as util
+
+class Scraper:
+    """
+    A static class for scraping functions
+    """
+
+    @staticmethod
+    def get_symbol(symbol):
+        """
+        Get a company name given a stock symbol
+        @param symbol: The stock ticker for a given company
+        @return: The company name associated with `symbol`
+        """
+        symbol_list = requests.get("http://chstocksearch.herokuapp.com/api/{}".format(symbol)).json()
+
+        for x in symbol_list:
+            if x['symbol'] == symbol:
+                return x['company']
+
+        return ""
+
+    @staticmethod
+    def scrape(symbol, interval=None, debug=False):
+        """
+        Get stock data from Yahoo for a company with symbol `symbol` between a time `interval`
+        @param symbol: The stock ticker for a given company
+        @param interval: A tuple or array of date values in the format 'm-d-Y'. If None is provided, will
+        @param debug: A boolean flag whether to print debug statements or not
+        @return: An array of dictionaries representing rows of data. Ex:
+            [{'date': 'Mar 19, 2020', 'open': '137.38', 'high': '152.49', 'low': '130.85', 'close': '149.49', 'adj_close': '149.49', 'volume': '6,543,800'},
+             {'date': 'Mar 18, 2020', 'open': '150.00', 'high': '155.97', 'low': '135.41', 'close': '140.02', 'adj_close': '140.02', 'volume': '9,543,200'},
+            ]
+        """
+
+        if not symbol:
+            print("Invalid request! No stock symbol provided")
+            return
+
+        url = ""
+
+        if interval is None:
+            url = "https://finance.yahoo.com/quote/GS/history?p=" + symbol
+        else:
+            if interval[0] is None or interval[1] is None:
+                print("Invalid request! One of the intervals is None")
+                return
+
+            # convert date strings into epoch times
+            begin_date = round(datetime.datetime.strptime(interval[0], '%m-%d-%Y').timestamp())
+            end_date = round(datetime.datetime.strptime(interval[1], '%m-%d-%Y').timestamp())
+
+            url = "https://finance.yahoo.com/quote/" + str(symbol) + "/history?period1=" + str(
+                begin_date) + "&period2=" + str(end_date) + "&interval=1d&filter=history&frequency=1d"
+
+        if debug:
+            print("Querying URL: " + url)
+
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'lxml')
+
+        stocks = soup.findAll('tr', class_='BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)')
+        stock_data = []  # output
+
+        if stocks:
+            for stock in stocks:
+                stock_data_soup = stock.find_all('span')
+
+                if len(stock_data_soup) > 2:  # want to exclude "Dividend" entries
+                    stock_data.append({"date": stock_data_soup[0].text,
+                                       "open": stock_data_soup[1].text,
+                                       "high": stock_data_soup[2].text,
+                                       "low": stock_data_soup[3].text,
+                                       "close": stock_data_soup[4].text,
+                                       "adj_close": stock_data_soup[5].text,
+                                       "volume": stock_data_soup[6].text
+                                       })
+
+        return stock_data
+
+    @staticmethod
+    def update_symbol(symbol, interval, database_path, table_name, debug=False):
+        """
+        Updates the table table_name at the database specified by database_path
+        with all the data on Yahoo finance for symbol within the time specified
+        by interval
+
+        :param symbol: The stock ticker for a given company
+        :param interval: A tuple or array of date values in the format 'm-d-Y'.
+        :param database_path: rel path of database
+        :param table_name: name of table in which we will input data
+        :param debug: A boolean flag whether to print debug statements or not
+        """
+        stock_data = Scraper.scrape(symbol, interval)
+
+        if debug:
+            print(stock_data)
+
+        values = ['symbol', 'date', 'open', 'high', 'low', 'close', 'adj_close', 'volume']
+
+        # list of tuples (col_name, data_type) strings
+        table_data = [
+            ("symbol", "TEXT"),
+            ("date", "TEXT"),
+            ("open", "REAL"),
+            ("high", "REAL"),
+            ("low", "REAL"),
+            ("close", "REAL"),
+            ("adj_close", "REAL"),
+            ("volume", "REAL")
+        ]
+
+        util.create_table(table_name, values, table_data, database_path, debug)
+
+        for row in stock_data:
+
+            # values is a list of string literal values for this row/date
+            values = [
+                symbol,
+                row["date"],
+                row["open"],
+                row["high"],
+                row["low"],
+                row["close"],
+                row["adj_close"],
+                row["volume"]
+            ]
+
+            util.data_entry(table_name, values, table_data, database_path, debug)
+
+
+# Testing Methods for scraper:
+
+DATABASE_URL = "../../database/stocksInfo.db"
+
+data = Scraper.scrape("GS", ["2-17-2005", "3-20-2020"], True)
+print(data)
+
+# Scraper.update_symbol("GS", ["2-17-2005", "3-20-2020"], DATABASE_URL, "test_scraper_GS_01", False)
