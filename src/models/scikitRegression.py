c707ab7a2498ae2a176e46780eec2116936d3a64
Enrique Avina <rickyavina@icloud.com>
2020-04-11 01:37:21 -0700

    Added more categories to predict close price (we won't really have those things though). There's also cool analytics

--- src/models/scikitRegression.py
+++ src/models/scikitRegression.py
@@ -6,18 +6,20 @@ import pandas as pd
 import sqlite3
 import datetime as dt
 from sklearn import metrics
+import seaborn as sns
 
 DB_FILE_LOC = '../scraping/data/stockInfo.db'
 table_name = 'GS'
 
 conn = sqlite3.connect(DB_FILE_LOC)
-df = pd.read_sql_query("SELECT Timestamp, Open FROM " + table_name, conn)
+df = pd.read_sql_query("SELECT * FROM " + table_name, conn)
 conn.commit()
 conn.close()
 
-# split into X and Y. Use timestamp to predict open price
-X = df["Timestamp"].values.reshape(-1, 1)
-y = df["Open"].values.reshape(-1, 1)
+print(df.columns)
+# split into X and Y. Use everything but close to predict close
+X = df[['Timestamp', 'Open', 'High', 'Low', 'Volume']].values
+y = df["Close"].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
@@ -31,16 +33,33 @@ model = lm.fit(X_train, y_train)
 model.fit(X_train, y_train)
 y_pred = lm.predict(X_test)
 
+# df2 = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+# df1 = df2.head(25)
+# print(df2)
+
+f, ax = plt.subplots(figsize=(10, 6))
+corr = df.corr()
+hm = sns.heatmap(round(corr,2), annot=True, ax=ax, cmap="coolwarm",fmt='.2f',
+                 linewidths=.05)
+f.subplots_adjust(top=0.93)
+f.suptitle('Stock Correlation Heatmap', fontsize=14)
+
 # plotting predictions
-prediction_dates = [dt.datetime.fromtimestamp(ts) for ts in X_test.flatten()]
-plt.xticks(rotation=25)
-ax=plt.gca()
-ax.set_xlim(min(prediction_dates), max(prediction_dates))
-plt.scatter(prediction_dates, y_test, color='blue')
-plt.scatter(prediction_dates, y_pred, color='red')
-plt.xlabel("Open ($)")
-plt.ylabel("Dates")
-plt.legend(('y test', 'prediction'))
+g, ax2 = plt.subplots(figsize=(10, 6))
+ax2.scatter(y_test, y_pred)
+ax2.set_xlabel("Truth value")
+ax2.set_ylabel("Predictions")
+g.suptitle("Actual values vs predictions")
+
+prediction_dates = [dt.datetime.fromtimestamp(ts) for ts in X_test[:, 0]]
+h, ax3 = plt.subplots(figsize=(10, 6))
+ax3.scatter(prediction_dates, y_test)
+ax3.scatter(prediction_dates, y_pred)
+ax3.set_xlim(min(prediction_dates), max(prediction_dates))
+ax3.set_xlabel("Timestamp")
+ax3.set_ylabel("Close price ($)")
+ax3.legend((('y test', 'prediction')))
+h.suptitle("Timestamp vs Close price on predictions vs actual")
 
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
 print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
